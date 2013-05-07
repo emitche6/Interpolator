6c241f8968722f71136aa6df544eb1855da567d9
Eric Miller <emill18@u.rochester.edu>
2013-05-07 14:58:35 -0400

    Save?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
  
     respond_to do |format|
       if @user.save
-        format.html { redirect_to(:users, :notice => 'Registration successfull.') }
+        format.html { redirect_to(:interpolations, :notice => 'Registration successfull.') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
